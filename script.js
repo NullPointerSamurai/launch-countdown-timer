4d1247d7948cd9d94488c92a0204708ba663e963
3vilBonobo <iboutsika@gmail.com>
2024-03-29 11:13:28 +0200

    add countdown functionality

---
+++ script.js
@@ -0,0 +1,42 @@
+let countDownDate = new Date('Apr 3, 2024 20:37:25').getTime();
+function getRemainingTime() {
+  let currentTime = new Date().getTime();
+  let timeDifference = countDownDate - currentTime;
+  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  let getRemainingDays = () => {
+    let remainingDays = document.querySelectorAll('.days');
+    remainingDays.forEach((remainingDay) => {
+      remainingDay.innerHTML = days;
+    });
+  };
+  let getRemainingHours = () => {
+    let remainingHours = document.querySelectorAll('.hours');
+    remainingHours.forEach((remainingHour) => {
+      remainingHour.innerHTML = hours;
+    });
+  };
+
+  let getRemainingMinutes = () => {
+    let remainingMinutes = document.querySelectorAll('.minutes');
+    remainingMinutes.forEach((remainingMinute) => {
+      remainingMinute.innerHTML = minutes;
+    });
+  };
+  let getRemainingSeconds = () => {
+    let remainingSeconds = document.querySelectorAll('.seconds');
+    remainingSeconds.forEach((remainingSecond) => {
+      remainingSecond.innerHTML = seconds;
+    });
+  };
+  getRemainingDays();
+  getRemainingHours();
+  getRemainingMinutes();
+  getRemainingSeconds();
+}
+setInterval(getRemainingTime, 1000);
