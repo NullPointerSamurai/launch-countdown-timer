820d937b07e0aec0ba9b18165503865112a9af22
3vilBonobo <iboutsika@gmail.com>
2024-05-14 17:27:10 +0300

    add basic js

--- multi-step-form/script.js
+++ multi-step-form/script.js
@@ -1,3 +1,39 @@
+const userData = [];
+
+class User {
+  constructor(name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+  }
+}
+
+//Save user input in new Object
+const saveUserData = () => {
+  userData.push(
+    new User(
+      document.getElementById('name').value,
+      document.getElementById('email').value,
+      document.getElementById('phone').value
+    )
+  );
+};
+
+//Movie to next step
+const goNextStep = () => {
+  //prevent if input is not valid
+  if (!validateForm()) {
+    return false;
+  }
+};
+
+//Validate form
+const validateForm = () => {
+  let valid = true;
+  //check for empty inputs
+};
+
+//toggle plan for step 2
 const planArray = document.querySelectorAll('.plan');
 
 const togglePlan = (event) => {
